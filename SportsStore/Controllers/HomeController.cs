abc273fe918620f9e4e3201eb1b4cbdffdda04fe
Brayl Amaquin <brayljamesamaquin@gmail.com>
2024-05-17 17:50:18 +0800

    added razor pages

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
@@ -13,26 +14,31 @@ namespace SportsStore.Controllers
             repository = repo;
         }
 
-        public ViewResult Index(string category, int productPage = 1)
-        {
-            //return View(new ProductsListViewModel
-            //{
-            //    Products = repository.Products
-            //        .Where(p => category == null || p.Category == category)
-            //        .OrderBy(p => p.ProductID)
-            //        .Skip((productPage - 1) * PageSize)
-            //        .Take(PageSize),
-            //    PagingInfo = new PagingInfo
-            //    {
-            //        CurrentPage = productPage,
-            //        ItemsPerPage = PageSize,
-            //        TotalItems = category == null ? repository.Products.Count() : repository.Products.Where(e =>
-            //                e.Category == category).Count()
-            //    },
-            //    CurrentCategory = category
-            //});
+        //public ViewResult Index(int productPage = 1)
+        //    => View(repository.Products
+        //        .OrderBy(p => p.ProductID)
+        //        .Skip((productPage - 1) * PageSize)
+        //        .Take(PageSize));          
 
-            return View(repository.Products.OrderBy(p => p.ProductID).Skip((productPage - 1) * PageSize).Take(PageSize));
+        public ViewResult Index(string? category, int productPage = 1)
+        {
+            return View(new ProductsListViewModel
+            {
+                Products = repository.Products
+                    .Where(p => category == null || 
+                        p.Category == category)
+                    .OrderBy(p => p.ProductID)
+                    .Skip((productPage - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = category == null ? repository.Products.Count() : repository.Products.Where(e =>
+                            e.Category == category).Count()
+                },
+                CurrentCategory = category
+            });
         }
     }
 }
--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -11,14 +11,35 @@ builder.Services.AddDbContext<StoreDBContext>(opts =>
         builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
 });
 
-builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();;
 
+builder.Services.AddRazorPages();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
-
 app.UseStaticFiles();
+app.UseSession();
+
+app.MapControllerRoute("catpage",
+    "{category}/Page{productPage:int}",
+    new {Controller = "Home", Action = "Index" });
+
+app.MapControllerRoute("page",
+    "Page{productPage:int}",
+    new { Controller = "Home", Action = "Index" , productPage = 1});
+
+app.MapControllerRoute("category",
+    "{category}",
+    new { Controller = "Home", action = "Index", productPage = 1 });
+
+app.MapControllerRoute("pagination",
+    "Products/Page{productPage}",
+    new {Controller = "Home", action = "Index"});
+
 app.MapDefaultControllerRoute();
+app.MapRazorPages();
 
 SeedData.EnsurePopulated(app);
 
